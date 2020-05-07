58f42d837b088b2dc4ad4b5fba5a300d8176e835
marker <max3472@gmail.com>
2020-05-07 07:53:17 +0300

    Добавлен метод рулетки choice

--- kursach/main.cpp
+++ kursach/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 float f1(float);
 float f2(float);
+int choice(int, int);
 
 float a = 0.2;
 float b = 2;
@@ -11,7 +13,26 @@ float b = 2;
 int main(){ 
     cout << f2(3.0) << endl;
     //printf("%f\n", f2(3.0));
+    cout << choice(10,40);
     return 0;
+}
+
+int choice(int p1, int p2){
+    
+    int s = p1 + p2;
+    srand(time(NULL));
+    int random = rand() % s + 1;
+    cout << random << endl;
+
+    if (random <= p1){
+        return 1;
+    }
+    else{
+        return 2;
+    }
+    
+    
+
 }
 
 float f1(float x){
